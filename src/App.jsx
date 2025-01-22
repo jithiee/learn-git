2f69ff9a2506d3bfcb950d5175fa0103b633fd42
Sarang <sarang00005@gmail.com>
2025-01-22 15:28:31 +0530

    added-content-in fd

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 
 import './App.css'
 import Header from './pages/home/components/header/Header'
-
-
+import Footer from './pages/home/components/footer/Footer'
+import Looking from './pages/home/components/looking/Looking'
 
 function App() {
   
 
   return (
     <>
-      {/* <h1> hii</h1>
-      <button> click me </button>
-      <p>helllllll</p> */}
-
-  
-
-
-    <Header/>
+      <Header/>
+      <Looking/>
+      <Footer/>
     </>
   )
 }
